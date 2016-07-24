d13715554c21e85c6c52e0ac17d9ec57550aefd9
kuramu1108 <colagarychen@gmail.com>
2016-07-24 22:05:36 +1000

    location search

--- js/search.js
+++ js/search.js
@@ -1,6 +1,15 @@
 
 var map;
 var infowindow;
+var totalResults;
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(radarSearch);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
 
 function initMap() {
     var pyrmont = {lat: -33.883576, lng: 151.200505};
@@ -41,7 +50,7 @@ function createMarker(place) {
     });
 }
 // 191 results
-function radarSearch() {
+function radarSearchtest() {
     var pyrmont = {lat: -33.883576, lng: 151.200505};
 
     map = new google.maps.Map(document.getElementById('map'), {
@@ -55,25 +64,75 @@ function radarSearch() {
     location: pyrmont,
     radius: 500,
     type: ['restaurant']
-    }, getDetail);
+    }, processResults);
 }
 
-function getDetail(results, status) {
+function radarSearch(position) {
+    var pyrmont = {
+        lat: position.coords.latitude, 
+        lng: position.coords.longitude
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.radarSearch({
+    location: pyrmont,
+    radius: 500,
+    type: ['restaurant']
+    }, processResults);
+}
+
+function processResults(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 40; i < results.length; i++) {
-            var request = {
-                placeId: results[i].place_id
-            };
-            var service = new google.maps.places.PlacesService(map);
-            service.getDetails(request, callbackDetail);
-        }
+        totalResults = results;
+        getRandomDetail();
     }
 }
 
-function callbackDetail(place, status){
+function getRandomDetail() {
+    var randomPick = random(0, totalResults.length - 1);
+    var place = totalResults[randomPick];
+    
+    var request = {
+        placeId: place.place_id
+    }
+    // alert('try');
+    var service = new google.maps.places.PlacesService(map);
+    service.getDetails(request, filterDetail);
+}
+
+function filterDetail (place, status){
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-        createMarker(place);
+        var allGood = true;
+        // check opening hour detail exist
+        // if (place.opening_hours) {
+        //     // if not open now
+        //     alert(place.opening_hours.open_now);
+        //     if (!place.opening_hours.open_now) {
+        //         allGood = false;
+        //     }
+        // } else allGood = false;
+
+
+        // if all good, create marker
+        allGood ? createMarker(place) : getRandomDetail();
     }
 }
+
+function random (min, max) {
+    return Math.floor((Math.random() * max) + min);
+}
+
 $('#test').click(initMap);
-$('#random').click(radarSearch);
+$('#random').click(radarSearchtest);
+$('#current').click(getLocation);
+
+// jquery ondocument ready
+// $(function() {
+    
+// });
